eaa4cf4294d2697a1659da9a8bbbc7837dd3598c
Huynh Quang Phuc <quangphuc.huynh95@gmail.com>
2023-10-26 12:28:10 +0700

    update

---
+++ src/content-script/code_scaleflex_cloud.ts
@@ -0,0 +1,6 @@
+const headerMessage = document.querySelector('.header-message')
+if (headerMessage && headerMessage instanceof HTMLElement && headerMessage.innerText === 'Welcome to Scaleflex Code') {
+  headerMessage.style.background = '#002000'
+  headerMessage.style.color = '#fff'
+}
+
