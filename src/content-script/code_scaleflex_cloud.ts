e14e78103bba65a268b5814c0306187479f2275a
Huynh Quang Phuc <quangphuc.huynh95@gmail.com>
2023-10-30 16:13:16 +0700

    update css instead of js

--- src/content-script/code_scaleflex_cloud.ts
+++ src/content-script/code_scaleflex_cloud.ts
@@ -1,6 +1,1 @@
-const headerMessage = document.querySelector('.header-message')
-if (headerMessage && headerMessage instanceof HTMLElement && headerMessage.innerText === 'Welcome to Scaleflex Code') {
-  headerMessage.style.background = '#002000'
-  headerMessage.style.color = '#fff'
-}
-
+console.log("injected css");
